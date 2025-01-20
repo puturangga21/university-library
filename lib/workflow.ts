252581e7cc718f9c56ac05d462d584b0b1f87dca
puturangga21 <puturangga21@gmail.com>
2025-01-20 18:12:01 +0800

    implement qstash workflow for user onboarding

--- lib/workflow.ts
+++ lib/workflow.ts
@@ -1,4 +1,5 @@
 import { Client as WorkflowClient } from "@upstash/workflow";
+import { Client as QStashClient, resend } from "@upstash/qstash";
 import config from "./config";
 
 export const workflowClient = new WorkflowClient({
@@ -6,4 +7,29 @@ export const workflowClient = new WorkflowClient({
   token: config.env.upstash.qstashToken,
 });
 
-export default workflowClient;
+const qstashClient = new QStashClient({
+  token: config.env.upstash.qstashToken,
+});
+
+export const sendEmail = async ({
+  email,
+  subject,
+  message,
+}: {
+  email: string;
+  subject: string;
+  message: string;
+}) => {
+  await qstashClient.publishJSON({
+    api: {
+      name: "email",
+      provider: resend({ token: config.env.resendToken }),
+    },
+    body: {
+      from: "JS Mastery <puturangga21@gmail.com>",
+      to: [email],
+      subject,
+      html: message,
+    },
+  });
+};
